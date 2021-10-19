6472ac54b97f67b8ae730e9b57e9537404a65c3e
Hardik Chemburkar <91974776+TRENCHhardik2042@users.noreply.github.com>
2021-10-19 15:15:10 +0530

    Area of a Circle
    It helps to find area of a circle with the help of radius input which is taken from the user

---
+++ Task1.1.py
@@ -0,0 +1,12 @@
+# WAP input radius of a circle and find its area 
+print ("Input the radius of Circle: ")
+radius = float(input())
+
+#Declaring the value of PI 
+PI = 3.141592653589793
+
+# Using the Area formula 
+Area = radius * radius * PI
+
+#printing the value of the computed area 
+print (Area)
